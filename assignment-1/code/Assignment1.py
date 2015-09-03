91a9ddd0a982fafba2a51e29b23f666cc0ade721
Deepak Gautam <deepugtm@gmail.com>
2015-09-02 21:53:56 -0500

    Minor Bug Fix :)

--- assignment-1/code/Assignment1.py
+++ assignment-1/code/Assignment1.py
@@ -4,7 +4,7 @@
 # <codecell>
 
 from random import shuffle
-no_team=7
+no_team=6
 with open('../data/students.tsv','rb') as stud_file:
     li=[entry.replace('\t', ' ').replace('\n','') for entry in stud_file]
 li=li[1:] #ignore first line of the file
@@ -12,8 +12,10 @@ li=li[1:] #ignore first line of the file
 # <codecell>
 
 shuffle(li)
-team_size =  len(li)/no_team if len(li)%no_team == 0 else (len(li)/no_team)+1
-teams=[li[i:i+team_size] for i in range(0,no_team*team_size,team_size)]
+team_size =  len(li)/no_team
+teams=[li[i:i+team_size] for i in range(0,team_size*no_team,team_size)]
+for i in range(team_size*no_team, len(li)):
+    teams[i-team_size*no_team].append(li[i])
 
 # <codecell>
 
